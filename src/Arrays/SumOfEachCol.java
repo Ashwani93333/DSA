cbbb1ed6757b5846c33d106a116366b1eb398265
Ashwani93333 <ashwanikumar93333@gmail.com>
2026-09-06 14:53:01 +0530

    SumOfEachCol 2D array ques.

---
+++ src/Arrays/SumOfEachCol.java
@@ -0,0 +1,27 @@
+package Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SumOfEachCol {
+    public static void main(String[] args) {
+        System.out.println(rowSum());
+    }
+
+    static List<Integer> rowSum(){
+        int[][] arr = {{1,2,3},{4,5,6},{7,8,9}};
+        int n = arr.length;
+        int m = arr[0].length;
+        int sum = 0;
+        List<Integer> list = new ArrayList<>();
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                sum += arr[j][i];
+
+            }
+            list.add(sum);
+            sum = 0;
+        }
+        return list;
+    }
+}
